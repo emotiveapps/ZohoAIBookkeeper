11fbeccfb483514175eaffaf362bc7ce656e4aeb
Andrew Ash <andrew@emotiveapps.com>
2026-02-09 15:51:35 -0500

    Removed Tuist cloud integration to reduce expenses for this project

--- Tuist.swift
+++ Tuist.swift
@@ -1,9 +1,18 @@
 import ProjectDescription
 
 let tuist = Tuist(
-    fullHandle: "andrewash/ZohoAIBookkeeper",
     project: .tuist(
         compatibleXcodeVersions: .upToNextMajor("26.0"),
         swiftVersion: "6.0"
     )
 )
+
+// Previous version that used Tuist Cloud (removed that feature to save on expenses)
+//
+// let tuist = Tuist(
+//    fullHandle: "andrewash/ZohoAIBookkeeper",
+//    project: .tuist(
+//        compatibleXcodeVersions: .upToNextMajor("26.0"),
+//        swiftVersion: "6.0"
+//    )
+// )
